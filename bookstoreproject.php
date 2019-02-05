1a488844ad65aef6dfcda250d2fce8056f51e104
SandeSydney <44159611+SandeSydney@users.noreply.github.com>
2019-02-04 22:18:17 -0800

    Add files via upload

---
+++ bookstoreproject.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Book store Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="responsive.css">
+  </head>
+  <body style="font-family:Verdana;color:#aaaaaa;">
+
+    <div style="background-color:#E6E6FA;padding:15px;text-align:center;">
+      <h1>SIDEE'S BOOK STORE</h1>
+    </div>
+
+    <div style="overflow:auto">
+      <div class="menu">
+        <a href="#">MONTHLY OFFERS!</a>
+        <a href="#">LATEST BOOKS</a>
+        <a href="login.php">LOGIN</a>
+        <a href="register.php">Register Here</a>
+      </div>
+
+      <div class="main">
+        <h2>Welcome</h2>
+        <p>This is a world of knowledge and fulfilment. Peruse through and find what you desire.</p>
+      </div>
+
+      <div class="right">
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+      </div>
+    </div>
+
+    <div style="background-color:#FFEBCD;text-align:center;padding:10px;margin-top:7px;">
+            <marquee style="color;black;">Designed By: Sansid Corporation &copy 2019</marquee>
+    </div>
+
+  </body>
+</html>
